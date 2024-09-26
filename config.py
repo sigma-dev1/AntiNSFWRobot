8eb7c98f62e11770313b8b2b2a9c01f918468124
sigma-dev1 <132310748+sigma-dev1@users.noreply.github.com>
2024-09-26 17:57:41 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config(object):
-    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    BOT_TOKEN = os.getenv("7548879127:AAGn6vgGmKpDlSCNNxH7m4mLBeP2lwkIhE4")
     API_ID = os.getenv("API_ID")
     API_HASH = os.getenv("API_HASH")
     DB_HOST = os.getenv("DB_HOST")
